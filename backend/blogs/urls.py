c83b2f0885d7cc3ed62378478bd4872b2eb1a681
Carlos Subero <carlos.lamole98@gmail.com>
2024-01-25 11:24:53 -0400

    Agregando respuesta de backend al eliminar comentarios y entradas y creando ultima vista del blog

--- backend/blogs/urls.py
+++ backend/blogs/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('get/', views.getBlogs),
+    path('get/<int:pk>/', views.getSoloBlogs),
+    path('post/', views.postBlog),
+    path('put/<int:pk>/', views.putBlog),
+    path('delete/<int:pk>/', views.deleteBlog),
+    path('comment/<int:pk)/', views.comment),
 ]
